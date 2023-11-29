447f776bdd1f68e032e5e3558e5027f72798b2b6
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-11-29 15:02:15 +0000

    Update Stack.h

--- Class/Stack.h
+++ Class/Stack.h
@@ -14,71 +14,20 @@ private:
     Node<T>* top;
 
 public:
-    Stack() 
-    : top(nullptr) 
-    {
-    }
-
-    ~Stack() 
-    {
-        while (!isEmpty()) 
-        {
-            pop(); // Remove all lingering elements in stack
-        }
-    }
+    Stack();
+    ~Stack();
 
     // Function to push a new element onto the stack
-    void push(const T& value) 
-    {
-        Node<T>* newNode = new Node<T>(value);
-        newNode->next = top;
-        top = newNode;
-    }
-
+    void push(const T& value);
     // Function to pop the top element from the stack
-    T pop() 
-    {
-        if (!isEmpty()) 
-        {
-            Node<T>* temp = top;
-            top = top->next;
-            return temp;
-        } 
-        else 
-        {
-            throw std::out_of_range("Stack is empty");
-        }
-    }
+    T pop();
 
     // Function to get the top element of the stack without removing it
-    T peek() const 
-    {
-        if (!isEmpty()) 
-        {
-            return top->data;
-        } 
-        else 
-        {
-            throw std::out_of_range("Stack is empty");
-        }
-    }
+    T peek() const;
 
     // Function to check if the stack is empty
-    bool isEmpty() const 
-    {
-        return top == nullptr;
-    }
+    bool isEmpty() const;
 
     // Function to get the current size of the stack
-    size_t size() const 
-    {
-        size_t count = 0;
-        Node<T>* current = top;
-        while (current != nullptr) 
-        {
-            count++;
-            current = current->next;
-        }
-        return count;
-    }
+    size_t size() const;
 };
