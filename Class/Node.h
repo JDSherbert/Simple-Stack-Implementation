741f45effc2093fa626e3b5b93404e4882907b95
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-11-29 14:36:38 +0000

    Create Node.h

---
+++ Class/Node.h
@@ -0,0 +1,12 @@
+// ©2023 JDSherbert. All rights reserved.
+
+#pragma once
+
+template <typename T>
+struct Node 
+{
+    T data;
+    Node* next;
+
+    Node(const T& value) : data(value), next(nullptr) {}
+};
